d854cdb44d881fa4d355dfc22aefb35cc2aaeb0a
Marco Bossi <marco.bossi90@gmail.com>
2018-08-02 17:49:50 +0200

    Structural Commit - SL

--- ProgettoPercorsoCircolareBossi/Circolare2018.SL/Models/ResourceModel.cs
+++ ProgettoPercorsoCircolareBossi/Circolare2018.SL/Models/ResourceModel.cs
@@ -7,5 +7,24 @@ namespace Circolare2018.SL.Models
 {
     public class ResourceModel
     {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Nullable<bool> IsAvaiable { get; set; }
+        public Nullable<bool> IsCP { get; set; }
+
+        public static ResourceModel MapModel(Entities.RESOURCE resource)
+        {
+            return new ResourceModel
+            {
+                ID = resource.ID,
+                UserName = resource.UserName,
+                Name = resource.Name,
+                Surname = resource.Surname,
+                IsAvaiable = resource.IsAvaiable,
+                IsCP = resource.IsCP
+            };
+        }
     }
 }
--- ProgettoPercorsoCircolareBossi/Circolare2018.SL/Models/TeacherModel.cs
+++ ProgettoPercorsoCircolareBossi/Circolare2018.SL/Models/TeacherModel.cs
@@ -7,5 +7,20 @@ namespace Circolare2018.SL.Models
 {
     public class TeacherModel
     {
+        public int ID { get; set; }
+        public int ID_Resource { get; set; }
+        public int ID_Course { get; set; }
+        public string Notes { get; set; }
+
+        public static TeacherModel MapModel(Entities.TEACHING teaching)
+        {
+            return new TeacherModel
+            {
+                ID = teaching.ID,
+                ID_Resource = teaching.ID_Resource,
+                ID_Course = teaching.ID_Course,
+                Notes = teaching.Notes
+            };
+        }
     }
 }
