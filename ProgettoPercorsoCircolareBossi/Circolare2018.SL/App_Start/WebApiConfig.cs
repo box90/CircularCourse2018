e08925b85e5d1a9850e349efe883181464c036ab
Marco Bossi <marco.bossi90@gmail.com>
2018-08-07 11:36:58 +0200

    Structure Commit

--- ProgettoPercorsoCircolareBossi/Circolare2018.SL/App_Start/WebApiConfig.cs
+++ ProgettoPercorsoCircolareBossi/Circolare2018.SL/App_Start/WebApiConfig.cs
@@ -10,6 +10,7 @@ namespace Circolare2018.SL
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            //config.EnableCors();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
